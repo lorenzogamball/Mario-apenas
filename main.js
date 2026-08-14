598a9e0a25f1eb3890d8b71ba7447f512d58438b
lorenzogamball <lorenzo.gamballi@escola.pr.gov.br>
2026-08-14 12:29:12 +0000

    Atualizar index.html e main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+//Seleção de Elementos do HtML
+Const audioStart = new Audio('.Sound/audio_theme.mp3');
+Const audioGameOver = new Audio('./soung/audio_gameover.mp3');
+Const mario = document.queryselector('.mario');
+Const pipe = document.queryselector('.pipe')
+Const startButton = document.queryselector('.startButton')
+Const restarButton = d ocument.queryselector('.restarButton')
+Const gameOver = document.queryselector('.gameOver')
+//Variaveis de Controle do Jogo
+Let gameInterval = null;
+//Funções para Iniciar o Jogo
+Const startGame = () => {}
+pipe.classlits.add('pipe-animation')
