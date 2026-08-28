43f875e6fee35d951285c6e3bb4e53f6ce86155d
lorenzogamball <lorenzo.gamballi@escola.pr.gov.br>
2026-08-28 11:07:05 +0000

    Corrigir declarações do jogo

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //Seleção de Elementos do HtML
-Const audioStart = new Audio('.Sound/audio_theme.mp3');
-Const audioGameOver = new Audio('./soung/audio_gameover.mp3');
+const audioStart = new Audio('.Sound/audio_theme.mp3');
+const audioGameOver = new Audio('./soung/audio_gameover.mp3');
 Const mario = document.queryselector('.mario');
-Const pipe = document.queryselector('.pipe')
-Const startButton = document.queryselector('.startButton')
-Const restarButton = d ocument.queryselector('.restarButton')
-Const gameOver = document.queryselector('.gameOver')
+Const pipe = document.queryselector('.pipe');
+Const startButton = document.queryselector('.startButton');
+Const restarButton = d ocument.queryselector('.restarButton');
+const gameOver = document.queryselector('.gameOver');
+
 //Variaveis de Controle do Jogo
 Let gameInterval = null;
+
 //Funções para Iniciar o Jogo
-Const startGame = () => {}
+const startGame = () => {}
 pipe.classlits.add('pipe-animation')
+pipe.style.left=
